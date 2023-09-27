3c2fa028bf65ac57bb80429b4c1fd7c6c81ad55d
PavelYudin <u-p.91@mail.ru>
2023-09-27 19:12:50 +0300

    1st task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,17 @@
 function sum(a, b) {
   /* ваш код */
+  function checkIsNumber(n) {
+    if(Number.isInteger(n) && n % 1 === 0) {
+      return true;
+    }
+    
+    throw new TypeError("")
+  }
+
+  if(checkIsNumber(a) && checkIsNumber(b)) {
+    return a + b;
+  } 
+
 }
 
 module.exports = sum;
