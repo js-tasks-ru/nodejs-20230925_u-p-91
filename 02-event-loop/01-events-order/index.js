a9976e39d78ffb52672bbaa2df5f0c58ba71af4b
PavelYudin <u-p.91@mail.ru>
2023-10-04 22:47:35 +0300

    2st task

--- 02-event-loop/01-events-order/index.js
+++ 02-event-loop/01-events-order/index.js
@@ -1,23 +1,31 @@
+const Log = require("./writeLog.js");
+
 const intervalId = setInterval(() => {
   console.log('James');
+  Log.getInstance().write('James');
 }, 10);
 
 setTimeout(() => {
   const promise = new Promise((resolve) => {
     console.log('Richard');
+    Log.getInstance().write('Richard');
     resolve('Robert');
   });
 
   promise
       .then((value) => {
         console.log(value);
+        Log.getInstance().write(value);
 
         setTimeout(() => {
           console.log('Michael');
+          Log.getInstance().write('Michael');
+          Log.getInstance().end();
 
           clearInterval(intervalId);
         }, 10);
       });
 
   console.log('John');
+  Log.getInstance().write('John');
 }, 10);
