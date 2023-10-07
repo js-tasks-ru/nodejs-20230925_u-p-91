c1247804a356d4901bacf97988b56b6e0865c499
PavelYudin <u-p.91@mail.ru>
2023-10-07 22:16:45 +0300

    stream: solution 2st

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,36 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this._encoding = options.encoding;
+    this.line = "";
   }
 
-  _transform(chunk, encoding, callback) {
+  async _transform(chunk, encoding, callback) {
+    
+    let data = chunk.toString(this._encoding);
+
+    if(~data.indexOf(os.EOL)) {
+      let arr = data.split(os.EOL);
+
+      for(let i = 0; i < arr.length - 1; i++) {
+        this.line = this.line + arr[i];
+        this.push(this.line);
+        this.line = "";
+      }
+
+      this.line += arr[arr.length - 1];
+
+    } else {
+      this.line += data;
+    }
+
+
+    callback()
   }
 
   _flush(callback) {
+    callback(null, this.line);
   }
 }
 
