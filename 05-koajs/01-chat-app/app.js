4fe3feedd7025b03b9e5f5389375bc43d2614548
PavelYudin <u-p.91@mail.ru>
2023-10-25 16:37:49 +0300

    solution 5st

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -6,12 +6,42 @@ app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
+
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+const store = new Map();
+
+router.get('/subscribe', async function(ctx, next) {
+    // const id = ctx.querystring.split("=")[1]; 
+    ctx.req.on('close', () => {
+        store.delete(ctx)
+    }); 
+
+    await new Promise(function(resolve, reject) {
+        store.set(ctx, resolve);
+    });
+
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post('/publish', async function(ctx, next) {
+    const message = ctx.request.body.message;
+
+    if(message === undefined) {
+        return ctx.status = 400;
+    }
+  
+    for (let arr of store) {
+        const ctx = arr[0];
+        const resolve = arr[1];
+
+        ctx.status = 200;
+        ctx.body = message;
+        resolve()
+    }
+
+    store.clear();
+    ctx.status = 200;
+
 });
 
 app.use(router.routes());
