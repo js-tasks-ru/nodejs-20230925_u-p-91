3f32d615f5c8670cadfa7062f6da186990d49e9a
PavelYudin <u-p.91@mail.ru>
2023-11-02 22:18:53 +0300

    solution 1st

--- 06-mongodb-mongoose/01-schema-model/models/Category.js
+++ 06-mongodb-mongoose/01-schema-model/models/Category.js
@@ -2,9 +2,24 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    }
 });
 
+const subcategoriesModel = connection.model('Subcategorie', subCategorySchema);
+
 const categorySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },      
+    subcategories: [subCategorySchema],
+    // subcategories: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Subcategorie'
+    // }]
 });
 
 module.exports = connection.model('Category', categorySchema);
