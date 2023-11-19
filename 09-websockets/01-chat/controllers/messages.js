605a55f09be7a229ad144c486e01d91a1b7623bd
PavelYudin <u-p.91@mail.ru>
2023-11-19 21:30:19 +0300

    solution 1st

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,15 @@
 const Message = require('../models/Message');
+const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {};
+  if(!ctx.user) {
+    return ctx.throw(401, 'Пользователь не залогинен');
+  }
+  
+  try {
+    const messages = await Message.find({chat: ctx.user._id.toString()}).limit(20);
+    ctx.body = {messages: messages.map(mapMessage)};
+  } catch(err) {
+    throw err;
+  }
 };
--- 09-websockets/01-chat/socket.js
+++ 09-websockets/01-chat/socket.js
@@ -4,14 +4,41 @@ const Session = require('./models/Session');
 const Message = require('./models/Message');
 
 function socket(server) {
-  const io = socketIO(server);
+  const io = socketIO(server, {allowEIO3: true});
 
   io.use(async function(socket, next) {
-    next();
+    const token = socket.handshake.query.token;
+    try {
+      if(!token) {
+        return next(new Error("anonymous sessions are not allowed"));
+      }
+      
+      const sessionUser = await Session.findOne({ token: token }).populate('user');
+      if(!sessionUser) {
+        return next(new Error("wrong or expired session token"));
+      }
+      socket.user = sessionUser.user;
+
+      next();
+
+    } catch(err) {
+      next(new Error(err.message))
+    }
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      try {
+        await Message.create({
+          date: new Date(),
+          text: msg,
+          chat: socket.user._id.toString(),
+          user: socket.user.displayName
+        });
+      }catch(err) {
+        next(new Error(err.message))
+      }
+    });
   });
 
   return io;
